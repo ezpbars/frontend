a6d1fa66a40b0fd41376fbb4917d0ea802a35049
Amanda Moore <amanda.joy24@outlook.com>
2022-07-25 10:43:04 -0700

    add continuous deployment scripts

---
+++ scripts/auto/after_install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+install_nginx() {
+    cp scripts/nginx.repo /etc/yum.repos.d/nginx.repo
+    sudo -u ec2-user mkdir -p /home/ec2-user/logs
+
+    yum clean metadata
+    yum update -y
+    yum install -y nginx
+    nginx -t && nginx
+    nginx -s quit
+}
+
+install_nginx_if_necessary() {
+    if [ ! -f /etc/yum.repos.d/nginx.repo ]
+    then
+        install_nginx
+    fi
+}
+
+update_nginx_config() {
+    cp scripts/nginx.conf /etc/nginx/nginx.conf
+}
+
+update_website_code() {
+    if [ ! -d venv ]
+    then
+        python3 -m venv venv
+    fi
+    . venv/bin/activate
+    python -m pip install -U pip
+    pip install -r requirements.txt
+    vanillaplusjs build --no-symlinks
+    deactivate
+    rsync -avu --delete out/ /var/www
+}
+
+install_nginx_if_necessary
+update_nginx_config
+update_website_code
